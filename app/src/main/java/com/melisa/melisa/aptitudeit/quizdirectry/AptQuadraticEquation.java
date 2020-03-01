363238c8a5c9a38d2e2a569ed1b440d3ef3fac0b
rincemathew <rincemathewmanattu@gmail.com>
2020-03-01 13:34:33 +0530

    added quadratic equation ad simplification 1/3/20

--- app/src/main/java/com/melisa/melisa/aptitudeit/quizdirectry/AptQuadraticEquation.java
+++ app/src/main/java/com/melisa/melisa/aptitudeit/quizdirectry/AptQuadraticEquation.java
@@ -3,26 +3,28 @@ package com.melisa.melisa.aptitudeit.quizdirectry;
 public class AptQuadraticEquation {
     private String mQuestions [] = {
             "This is some test",
-            "quadratic equation",
-            "",
-            "",
-            "",
-            "",
-            "",
-            ""
+            "I. x\u00B2 - x - 42 = 0,\n" +
+                    "II. y\u00B2 - 17y + 72 = 0 to solve both the equations to find the values of x and y?",
+            "I. x\u00B2 + 9x + 20 = 0,\n" +
+                    "II. y\u00B2 + 5y + 6 = 0 to solve both the equations to find the values of x and y?",
+            "Find the roots of quadratic equation: 2x\u00B2 + 5x + 2 = 0?",
+            "Find the roots of quadratic equation: x\u00B2 + x - 42 = 0?",
+            "Find the roots of quadratic equation: 3x\u00B2 - 7x - 6 = 0?",
+            "Find the value of a/b + b/a, if a and b are the roots of the quadratic equation x\u00B2 + 8x + 4 = 0?",
+            " A man could buy a certain number of notebooks for Rs.300. If each notebook cost is Rs.5 more, he could have bought 10 notebooks less for the same amount. Find the price of each notebook?"
     };
 
 
 
     private String mChoices [][] = {
             {"Fruit", "Leaves", "Seeds","Leaves"},
-            {"","","",""},
-            {"","","",""},
-            {"","","",""},
-            {"","","",""},
-            {"","","",""},
-            {"","","",""},
-            {"","","",""}
+            {"If x < y","If x > y","If x ≤ y","If x ≥ y"},
+            {"If x < y","If x > y","If x ≤ y","If x ≥ y"},
+            {"-2, -1/2","4, -1","4, 1","-2, 5/2"},
+            {"-6, 7","14, -3","-8, 7","-7, 6"},
+            {"-6, 3","3, -2/3","-5, 2","-9, 2"},
+            {"15","14","26","24"},
+            {"10","8","20","18"}
     };
 
 
@@ -40,7 +42,7 @@ public class AptQuadraticEquation {
 
 
 
-    private String mCorrectAnswers[] = {"Leaves","","","","","","",""};
+    private String mCorrectAnswers[] = {"Leaves","If x < y","If x < y","-2, -1/2","-7, 6","3, -2/3","14","10"};
 
 
     public int getLength()  {return mQuestions.length;}
--- app/src/main/java/com/melisa/melisa/aptitudeit/quizdirectry/AptSimplification.java
+++ app/src/main/java/com/melisa/melisa/aptitudeit/quizdirectry/AptSimplification.java
@@ -3,26 +3,26 @@ package com.melisa.melisa.aptitudeit.quizdirectry;
 public class AptSimplification {
     private String mQuestions [] = {
             "This is some test",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            ""
+            "In a regular week, there are 5 working days and for each day, the working hours are 8. A man gets Rs. 2.40 per hour for regular work and Rs. 3.20 per hours for overtime. If he earns Rs. 432 in 4 weeks, then how many hours does he work for ?",
+            "Free notebooks were distributed equally among children of a class. The number of notebooks each child got was one-eighth of the number of children. Had the number of children been half, each child would have got 16 notebooks. Total how many notebooks were distributed ?",
+            "A man has some hens and cows. If the number of heads be 48 and the number of feet equals 140, then the number of hens will be:",
+            "David gets on the elevator at the 11th floor of a building and rides up at the rate of 57 floors per minute. At the same time, Albert gets on an elevator at the 51st floor of the same building and rides down at the rate of 63 floors per minute. If they continue travelling at these rates, then at which floor will their paths cross ?" ,
+            "One-third of Rahul's savings in National Savings Certificate is equal to one-half of his savings in Public Provident Fund. If he has Rs. 1,50,000 as total savings, how much has he saved in Public Provident Fund ?",
+            "A fires 5 shots to B's 3 but A kills only once in 3 shots while B kills once in 2 shots. When B has missed 27 times, A has killed:",
+            "To fill a tank, 25 buckets of water is required. How many buckets of water will be required to fill the same tank if the capacity of the bucket is reduced to two-fifth of its present "
     };
 
 
 
     private String mChoices [][] = {
             {"Fruit", "Leaves", "Seeds","Leaves"},
-            {"","","",""},
-            {"","","",""},
-            {"","","",""},
-            {"","","",""},
-            {"","","",""},
-            {"","","",""},
-            {"","","",""}
+            {"160","175","180","195"},
+            {"256","432","512","None of these"},
+            {"23","24","25","26"},
+            {"30","31","32","33"},
+            {"Rs. 30,000","Rs. 50,000","Rs. 60,000","Rs. 70,000"},
+            {"30 birds","60 birds","72 birds","90 birds"},
+            {"10","35","62.5","Cannot be determined"}
     };
 
 
@@ -40,7 +40,7 @@ public class AptSimplification {
 
 
 
-    private String mCorrectAnswers[] = {"Leaves","","","","","","",""};
+    private String mCorrectAnswers[] = {"Leaves","175","512","26","30","Rs. 60,000","30 birds","62.5"};
 
 
     public int getLength()  {return mQuestions.length;}
